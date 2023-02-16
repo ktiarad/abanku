dec12ffc0cefd31cd2ea1fcce485ce1a8ecfc7f8
ktiarad <tirdewa@gmail.com>
2023-02-16 23:21:51 +0700

    change constants

--- configs/constants.go
+++ configs/constants.go
@@ -1,6 +1,18 @@
 package configs
 
 const (
+	// SYS
+	DbMaxConn     = 100
+	DbMaxIdleConn = 4
+
+	// DATABASE
+	DbHost = "<your_setting>"
+	DbPort = "<your_setting>"
+	DbUser = "<your_setting>"
+	DbPass = "<your_setting>"
+	DbName = "<your_setting>"
+
+	// BANK
 	MINIMUM_BALANCE         = 100000
 	ADMIN_FEE               = 15000
 	INTEREST_PERCENTAGE     = 0.5
--- configs/db.go
+++ configs/db.go
@@ -8,21 +8,15 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	DB_HOST = "<your_config>"
-	DB_PORT = "<your_config>"
-	DB_USER = "<your_config>"
-	DB_PASS = "<your_config>"
-	DB_NAME = "<your_config>"
-)
-
 func ConnectDB() *sql.DB {
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", DbHost, DbPort, DbUser, DbPass, DbName)
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		panic(err)
 	}
+
+	db.SetMaxOpenConns(DbMaxConn)
 
 	log.Default().Println("Connection db success")
 
